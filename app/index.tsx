b6353cb7413d2f177a61158e81acfc664cdabfb0
cbillard <cbillard@com-company.fr>
2024-09-11 11:31:43 +0200

    definitions of theme colors

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { ThemedText } from "@/components/ThemedText";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const colors = useThemeColors()
+  return (
+    <SafeAreaView style={[styles.container, { backgroundColor: colors.tint }]}>
+      <ThemedText variant="headline" color="grayWhite">Pokédex</ThemedText>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
